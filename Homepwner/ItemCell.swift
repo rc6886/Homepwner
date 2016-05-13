8d9504bf88e947a2b0d2d21956fa829a4a62ed15
Rodney Coleman Jr <rc6886@gmail.com>
2016-05-12 20:11:35 -0500

    Adding custom UITableViewCell.

---
+++ Homepwner/ItemCell.swift
@@ -0,0 +1,14 @@
+//
+//  ItemCell.swift
+//  Homepwner
+//
+//  Created by Rodney Coleman Jr. on 5/12/16.
+//  Copyright © 2016 rc6886. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ItemCell: UITableViewCell {
+    
+}
